39f32ef6d081397afaf84f21cc90fbd97b89a5e2
pcion <patrick.ciongoli@gmail.com>
2022-06-03 05:35:15 -0400

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,20 @@
+// Post model
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/config");
+
+// Create Post model
+class Post extends Model {}
+
+// Post
+Post.init(
+   {
+      title: DataTypes.STRING,
+      body: DataTypes.STRING,
+   },
+   {
+      sequelize,
+   }
+);
+
+// export
+module.exports = Post;
