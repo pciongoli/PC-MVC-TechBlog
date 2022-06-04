bdc12cb3cf36e2d024ee05d750f2c6c209a557ec
pcion <patrick.ciongoli@gmail.com>
2022-06-04 04:25:47 -0400

    added public/js functions

---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+// async function to handle signup form
+async function signupFormHandler(event) {
+   event.preventDefault();
+
+   const username = document.querySelector("#username-signup").value.trim();
+   const password = document.querySelector("#password-signup").value.trim();
+
+   if (username && email && password) {
+      const response = await fetch("/api/users", {
+         method: "post",
+         body: JSON.stringify({
+            username,
+            email,
+            password,
+         }),
+         headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+         document.location.replace("/dashboard/");
+      } else {
+         alert(response.statusText);
+      }
+   }
+}
+
+document
+   .querySelector(".signup-form")
+   .addEventListener("submit", signupFormHandler);
