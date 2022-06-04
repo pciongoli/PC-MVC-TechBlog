0456dee7fcc7e5f8cd2518947d65d21307f2f693
pcion <patrick.ciongoli@gmail.com>
2022-06-04 01:17:18 -0400

    updated dashboard/home-routes. updaetd handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,5 +3,58 @@ const router = require("express").Router();
 const { Post } = require("../models/");
 const withAuth = require("../utils/auth");
 
+// define each dashboard-route
+
+// Get route for main page
+router.get("/", withAuth, (req, res) => {
+   Post.findAll({
+      where: {
+         // using the ID from the session
+         userId: req.session.userId,
+      },
+   })
+      .then((dbPostData) => {
+         // serialize the data before passing it to the template
+         const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+         // Send 'loggedIn' session variable to 'homepage' template
+         res.render("all-posts", {
+            layout: "dashboard",
+            posts,
+         });
+      })
+      .catch((err) => {
+         console.log(err);
+         res.redirect("login");
+      });
+});
+
+router.get("/new", withAuth, (req, res) => {
+   res.render("new-post", {
+      layout: "dashboard",
+   });
+});
+
+// GET route for /edit/:id that renders the edit-post.handlebars
+// template by passing data from Post.findOne() query used in /post/:id
+router.get("/edit/:id", withAuth, (req, res) => {
+   Post.findByPk(req.params.id)
+      .then((dbPostData) => {
+         if (dbPostData) {
+            const post = dbPostData.get({ plain: true });
+
+            res.render("edit-post", {
+               layout: "dashboard",
+               post,
+            });
+         } else {
+            res.status(404).end();
+         }
+      })
+      .catch((err) => {
+         res.status(500).json(err);
+      });
+});
+
 // export
 module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models/");
+
+module.exports = router;
