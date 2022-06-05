6a5dbccb766fa4121ff7b3cc9e1e40e878d050c3
pcion <patrick.ciongoli@gmail.com>
2022-06-04 20:17:51 -0400

    resolving heroku errors

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const helpers = require('./utils/helpers');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const routes = require("./controller");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/config");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+   secret: "Super secret secret",
+   cookie: {},
+   resave: false,
+   saveUninitialized: true,
+   store: new SequelizeStore({
+      db: sequelize,
+   }),
 };
 
 app.use(session(sess));
@@ -28,12 +29,14 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+   app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+   });
 });
