f12397c61796f134ba2f147c8ed2ccceb192cc9b
pcion <patrick.ciongoli@gmail.com>
2022-06-04 20:31:59 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
-const routes = require("./controller");
-const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const routes = require("./controller");
+const sequelize = require("./config/config");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
    secret: "Super secret secret",
@@ -29,7 +29,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
