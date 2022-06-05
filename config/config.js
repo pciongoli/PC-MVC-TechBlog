e18d989bf4f16e389a9dd8481844af5696be15dc
pcion <patrick.ciongoli@gmail.com>
2022-06-04 20:21:06 -0400

    updated config file

--- config/config.js
+++ config/config.js
@@ -1,15 +1,12 @@
 const Sequelize = require("sequelize");
-
-// load environment variables into file
 require("dotenv").config();
 
-// create connection to our db
 const sequelize = process.env.JAWSDB_URL
    ? new Sequelize(process.env.JAWSDB_URL)
    : new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_PW,
+        process.env.DB_PASSWORD,
         {
            host: "localhost",
            dialect: "mysql",
